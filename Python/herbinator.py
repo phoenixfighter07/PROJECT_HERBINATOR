25dc3d67f72f7d32c94e03680cc26350d64bca69
Bunter <hmaas6@uw.edu>
2026-05-06 13:22:13 -0700

    part 1

--- Python/herbinator.py
+++ Python/herbinator.py
@@ -2,21 +2,44 @@
 # This file holds the code for the communications functions for the 
 # UWT Project Herbinator's communications file
 
+# Querying the esp for sensor state data periodically, and also manual functions
+# Send updates to override existing behavior 
 
-
-
+main()
+# 
 def send_command():
     print("I am working on it")
 
+#
+# def change_herbnet_name():
+#    print("Needs to be done")
 
-def change_herbnet_name():
-    print("Needs to be done")
+# #
+# def change_herbinator_name():
+#     print("to be worked on")
 
-def change_herbinator_name():
-    print("to be worked on")
-
+# #
+# display water level and manually water, water level reads as a percentage of total capacity
 def herbinator_water():
+
     print("to be worked on")
 
+# pause, can specify a time to be stopped, or stop indefinitely, method overload here for both conditions
+# pauses the pump manually
 def pause_herbinator():
-    print("outline function")
+    print("paused until manual resume")
+
+def pause_herbinator(time):
+    print("paused for 'time' minutes")
+
+def unpause_herbinator():
+    print("operations resumed")
+# read the temperature off the sensor, give a recommendation based off the temperature
+def get_temperature():
+    print("temperature")
+
+# def get_battery_percentage():
+#     print("battery level: 'level'")
+
+def get_herbinator_state():
+    print("state")
